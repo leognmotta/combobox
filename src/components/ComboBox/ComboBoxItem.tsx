f496ddfa39f7c48136e6bf9e8a527d4cb481acbc
leognmotta <mail@leomotta.me>
2023-04-06 08:08:25 -0300

    refact(ComboBoxItem): updadate props and return method

--- src/components/ComboBox/ComboBoxItem.tsx
+++ src/components/ComboBox/ComboBoxItem.tsx
@@ -1,21 +1,30 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useContext } from 'react'
+import { ComboBomContext } from '../../context/ComboBoxContext'
 
 export const COMPONENT_NAME = 'ComboBoxItem'
 
-/**
- * ComboBoxItem component
- *
- * @param {PropsWithChildren} props - Props of the component
- *
- * @returns {React.ReactElement} A React element
- *
- * @example
- * <ComboBoxItem>
- *   Content
- * </ComboBoxItem>
- */
-export const ComboBoxItem = ({ children }: PropsWithChildren) => {
-  return <>{children}</>
+interface ComboBoxItemProps {
+  value: string
+}
+
+export const ComboBoxItem = ({
+  children,
+  value,
+}: PropsWithChildren<ComboBoxItemProps>) => {
+  const { handleSelected, selectedKey } = useContext(ComboBomContext)
+
+  return (
+    <div
+      key={value}
+      role="option"
+      data-testid={`list-item-${value}`}
+      className="list-item"
+      onClick={() => handleSelected(value)}
+      aria-selected={selectedKey === value}
+    >
+      {children}
+    </div>
+  )
 }
 
 ComboBoxItem.displayName = COMPONENT_NAME
